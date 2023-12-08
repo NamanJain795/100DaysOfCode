40bcfbf424e5a63bba0ffec20ade3918bb45ca3f
NamanJain795 <141808883+NamanJain795@users.noreply.github.com>
2023-12-08 21:46:32 +0530

    Add files via upload

---
+++ Day8.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string ans;
+    void preorder(TreeNode* root){
+        if(root == NULL){
+            return;
+        }
+        ans += to_string(root -> val);
+        if(root -> left){
+            ans.push_back('(');
+            preorder(root -> left);
+            ans.push_back(')');
+        }
+        if(root -> right){
+            if(root -> left == NULL){
+                ans += "()";
+            }
+            ans.push_back('(');
+            preorder(root -> right);
+            ans.push_back(')');
+        }
+    }
+    string tree2str(TreeNode* root) {
+        preorder(root);
+        return ans;
+    }
+};
